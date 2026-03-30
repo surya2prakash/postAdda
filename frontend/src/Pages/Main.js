e251959c4d519b2f2a934d7c1177a74ab10deab1
surya2prakash <sv251171@gmail.com>
2026-03-30 13:53:28 +0530

    main Component

---
+++ frontend/src/Pages/Main.js
@@ -0,0 +1,68 @@
+import React, {  useContext, useEffect, useState } from 'react'
+import Navbar from '../Components/Navbar'
+
+import FooterSection from '../Components/FooterSection'
+import {Outlet } from 'react-router-dom'
+import Notification from '../Components/ProfileComponent/Notification'
+import { SocketContext } from '../ContextAndSocket/SocketContext';
+
+
+export default function Main() {
+    
+  const [showNotification,setShowNotification] = useState(false);
+    
+
+     function toggleNotification(){
+           setShowNotification(!showNotification);
+     }
+
+     
+
+     const[showNewNotificaion,setNewNotification] = useState([]);
+
+  const {currSocket,setCount} = useContext(SocketContext);
+
+  useEffect(()=>{
+
+     if(!currSocket){
+        return;
+     }
+
+    const handleback =(data)=>{
+           console.log(data);
+           setCount(prev=> prev+1);
+
+           setNewNotification(prev => [data, ...prev]);
+
+          
+   }
+        currSocket.on("send-like",handleback);
+        currSocket.on("notification",handleback);
+        return ()=>{
+            currSocket.off("send-like",handleback);
+        }
+   },[currSocket]);
+
+  
+  return (
+    <div className='relative h-screen  overflow-hidden grid grid-rows-[auto_1fr_auto] md:mx-[20%]  '>
+        
+           <nav className='sticky top-0 z-10 bg-slate-400 shadow '>
+            <Navbar toShowNotification={toggleNotification}  />
+            </nav>
+            <main className='min-h-full w-full overflow-y-auto' >
+             <Outlet/>
+            </main>
+            <footer className=' bg-neutral-400'>
+            <FooterSection/>
+            </footer>
+
+          {
+              showNotification && <div className='absolute top-16 right-5 z-50 max-h-44 w-72 bg-white shadow-xl border rounded-lg p-4 overflow-y-auto overflow-x-hidden'>
+                {showNewNotificaion.length === 0 ?(<Notification/>):(showNewNotificaion.map((data,index)=>(
+                    <Notification key={index} data={data}/>
+                )))}</div>
+          }        
+    </div>
+  )
+}
