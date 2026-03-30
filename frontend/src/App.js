81bcfe482a40d27b68f54bcb3a0960c3ae7de800
surya2prakash <sv251171@gmail.com>
2026-03-30 13:48:16 +0530

    App.js page

---
+++ frontend/src/App.js
@@ -0,0 +1,39 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./Pages/Login";
+import SignUp from "./Pages/SignUp";
+import ProtectedRoute from "./Pages/ProtectedRoute";
+
+import CreatePost from "./Components/CreatePost";
+import Main from "./Pages/Main";
+import Profile from "./Components/ProfileComponent/Profile";
+import MainSection from "./Components/MainSection";
+import Notification from "./Components/ProfileComponent/Notification";
+import ForgetPassword from "./Pages/ForgetPassword";
+
+
+function App() {
+  return (
+   <>
+   <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route path="/sign" element={<SignUp/>}/>
+        <Route path="/forget" element={<ForgetPassword/>}/>
+        <Route element={<ProtectedRoute/>}>
+        <Route element={<Main/>}>
+          <Route path="/main" element={<MainSection/>}></Route>
+          <Route path="/createpost" element={<CreatePost/>}></Route>
+          <Route path="/profile" element={<Profile/>}></Route>
+          <Route path="/profile/:id" element={<Profile/>}></Route>
+          </Route>
+          <Route path="/notification" element={<Notification/>}></Route>
+          
+        
+        </Route>
+   </Routes>
+    
+   </>
+  );
+}
+
+export default App;
