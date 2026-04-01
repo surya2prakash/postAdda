0168090cb592e0309a32efabaae0611f4c216e10
surya2prakash <sv251171@gmail.com>
2026-04-02 01:19:57 +0530

    socket.io

---
+++ frontend/src/ContextAndSocket/SocketContext.js
@@ -0,0 +1,76 @@
+import { createContext,  useContext,  useEffect, useState } from "react";
+import { AppContext } from "./ContextApi";
+
+import {io} from 'socket.io-client'
+
+
+
+
+  export const SocketContext = createContext(null);
+
+  const baseUrl = "http://localhost:5000"  ;
+
+
+
+  
+  
+  function SocketProvider ({children}){
+
+      const {isLogin} = useContext(AppContext);
+
+    const [currSocket,setSocket] = useState(null);
+    const [onlineUser,setOnlineUser] = useState(null);
+          
+    const[count,setCount] = useState(0);
+
+  
+
+   useEffect(()=>{
+
+    //  ager login true hai to  chlna nhi to waps jao
+    
+
+    if(!isLogin){
+        // waps jao 
+        return;
+    }
+
+    
+
+      const token = localStorage.getItem("token");
+
+   const socket = io(`${baseUrl}`,{
+      
+        auth:{
+                token
+              }
+          });
+
+          setSocket(socket);
+
+          socket.on("connect",()=>{
+                  
+          });
+
+          socket.on("userDetails",(data)=>{
+                setOnlineUser(data);
+          });
+           
+       
+
+          return  () =>{ socket.disconnect()
+                 setSocket(null);
+                 setOnlineUser(null);
+           };
+          
+   },[isLogin]);
+
+   
+       
+
+      return <SocketContext.Provider value={{currSocket,count,setCount}} >{children}</SocketContext.Provider>
+          }
+
+
+
+export default SocketProvider ;
