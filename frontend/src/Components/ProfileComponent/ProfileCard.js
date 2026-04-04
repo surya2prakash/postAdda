feecbc80ca8bca4a770f6ca0dc3e8b5c364cab4d
surya2prakash <sv251171@gmail.com>
2026-04-04 15:48:53 +0530

    Profile Card

---
+++ frontend/src/Components/ProfileComponent/ProfileCard.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { AiOutlineLike } from "react-icons/ai";
+
+export default function ProfileCard({post,onClick}) {
+ 
+  return (
+    <div className='relative cursor-pointer' onClick={onClick} >
+      {/* POST IMAGE */}
+             <div>
+                  {
+                     post?.postImage ? <img src={post?.postImage} alt='no' className='w-full h-52 object-fill' /> : post?.postImage ===null && <div>{post?.caption}</div>
+                  }  
+              </div>
+             <div className='absolute bottom-2 right-2 flex items-center gap-2 bg-black/60 text-white px-2 py-1 rounded'>
+              {/* TOTAL LIKE  */}
+              <div>{post?.totalLike}</div>
+              {/* THUMB ICON */}
+              <div><AiOutlineLike /></div>
+              </div> 
+         
+    </div>
+  )
+}
